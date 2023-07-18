a37ce4358cedb07e01848140fed136348094f1b1
Tathya Dixit <dixit.tathya2003@gmail.com>
2023-07-18 12:08:34 +0530

    fogot password

--- studentmanagement/settings.py
+++ studentmanagement/settings.py
@@ -130,3 +130,12 @@ MEDIA_ROOT = "/media/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'dixit.tathya2003@gmail.com' 
+EMAIL_HOST_PASSWORD = 'zywupucaptggwrql'
+EMAIL_USE_TLS = True
--- studentmanagement/urls.py
+++ studentmanagement/urls.py
@@ -3,12 +3,33 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from profiles import views
+from django.contrib.auth.views import PasswordResetView,PasswordResetDoneView,PasswordResetConfirmView,PasswordResetCompleteView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("pages.urls"),name='home'),
     path('',include("profiles.urls"),name='profiles'),
     path('login/',views.loginPage,name='loginpage'),
     path('logout/',views.logOut,name='logout'),
+    
+    path('reset_password/',
+    PasswordResetView.as_view(template_name="password_reset.html"),
+    name = "reset_password"),
+
+    path('reset_password_sent/',
+    PasswordResetDoneView.as_view(template_name = "password_reset_sent.html"),
+    name = "password_reset_done"),
+
+    path('reset/<uidb64>/<token>',
+    PasswordResetConfirmView.as_view(template_name = "password_reset_confirm.html"),
+    name = "password_reset_confirm"),
+
+    path('reset_password_complete/',
+    PasswordResetCompleteView.as_view(template_name = "password_reset_done.html"),
+    name = "password_reset_complete"),
+    
+
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
